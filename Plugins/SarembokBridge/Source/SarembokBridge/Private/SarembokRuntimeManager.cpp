64b193f8aa73208092ce3d1d9c244b758cba31a5
jetsontech <56722744+jetsontech@users.noreply.github.com>
2026-08-05 16:52:28 -0400

    Implement Sarembok runtime manager foundation

--- Plugins/SarembokBridge/Source/SarembokBridge/Private/SarembokRuntimeManager.cpp
+++ Plugins/SarembokBridge/Source/SarembokBridge/Private/SarembokRuntimeManager.cpp
@@ -1,34 +1,35 @@
 #include "SarembokRuntimeManager.h"
 
-
-FSarembokRuntimeManager::
-FSarembokRuntimeManager()
+FSarembokRuntimeManager::FSarembokRuntimeManager()
 {
 }
 
-
-
-FSarembokRuntimeManager&
-FSarembokRuntimeManager::Get()
+FSarembokRuntimeManager& FSarembokRuntimeManager::Get()
 {
     static FSarembokRuntimeManager Instance;
-
     return Instance;
 }
 
-
-
 void FSarembokRuntimeManager::Initialize()
 {
-    UE_LOG(
-        LogTemp,
-        Display,
-        TEXT("Sarembok Runtime Manager Initialized")
-    );
+    if (bInitialized)
+    {
+        return;
+    }
+
+    bInitialized = true;
+
+    UE_LOG(LogTemp, Display, TEXT("Sarembok Runtime Manager Initialized"));
 }
-
-
 
 void FSarembokRuntimeManager::Shutdown()
 {
+    if (!bInitialized)
+    {
+        return;
+    }
+
+    bInitialized = false;
+
+    UE_LOG(LogTemp, Display, TEXT("Sarembok Runtime Manager Shutdown"));
 }
