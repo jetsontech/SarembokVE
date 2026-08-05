3b9aca26cadca555d768e553f500a1b6c592933a
jetsontech <56722744+jetsontech@users.noreply.github.com>
2026-08-05 16:52:22 -0400

    Implement Sarembok runtime manager foundation

--- Plugins/SarembokBridge/Source/SarembokBridge/Public/SarembokRuntimeManager.h
+++ Plugins/SarembokBridge/Source/SarembokBridge/Public/SarembokRuntimeManager.h
@@ -2,21 +2,20 @@
 
 #include "CoreMinimal.h"
 
-
 class SAREMBOKBRIDGE_API FSarembokRuntimeManager
 {
-
 public:
 
     static FSarembokRuntimeManager& Get();
 
     void Initialize();
-
     void Shutdown();
 
+    bool IsInitialized() const { return bInitialized; }
 
 private:
 
     FSarembokRuntimeManager();
 
+    bool bInitialized = false;
 };
