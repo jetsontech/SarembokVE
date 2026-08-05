70a091406bd3076634d84abe0dc055a8436f96d7
jetsontech <56722744+jetsontech@users.noreply.github.com>
2026-08-05 17:13:13 -0400

    Add Sarembok bridge service manager

---
+++ Plugins/SarembokBridge/Source/SarembokBridge/Public/SarembokBridgeService.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SarembokMessage.h"
+#include "SarembokWebSocketClient.h"
+
+class SAREMBOKBRIDGE_API FSarembokBridgeService
+{
+public:
+
+    static FSarembokBridgeService& Get();
+
+    void Initialize();
+    void Shutdown();
+
+    void SendMessage(const FSarembokMessage& Message);
+
+    bool IsReady() const { return bReady; }
+
+private:
+
+    FSarembokBridgeService();
+
+    TSharedPtr<FSarembokWebSocketClient> WebSocketClient;
+
+    bool bReady = false;
+};
