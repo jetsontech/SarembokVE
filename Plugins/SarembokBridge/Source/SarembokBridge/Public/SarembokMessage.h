9fe1368ac82b0b16c4ae7f170aac206f3ee030c0
jetsontech <56722744+jetsontech@users.noreply.github.com>
2026-08-05 17:07:54 -0400

    Expand Sarembok message event envelope

--- Plugins/SarembokBridge/Source/SarembokBridge/Public/SarembokMessage.h
+++ Plugins/SarembokBridge/Source/SarembokBridge/Public/SarembokMessage.h
@@ -3,23 +3,27 @@
 #include "CoreMinimal.h"
 #include "SarembokMessage.generated.h"
 
-
 USTRUCT(BlueprintType)
 struct SAREMBOKBRIDGE_API FSarembokMessage
 {
-
     GENERATED_BODY()
-
 
 public:
 
-
-    UPROPERTY(BlueprintReadWrite)
+    UPROPERTY(BlueprintReadWrite, Category="Sarembok")
     FString Event;
 
+    UPROPERTY(BlueprintReadWrite, Category="Sarembok")
+    FString Source;
 
-    UPROPERTY(BlueprintReadWrite)
+    UPROPERTY(BlueprintReadWrite, Category="Sarembok")
     FString Data;
 
+    UPROPERTY(BlueprintReadWrite, Category="Sarembok")
+    double Timestamp = 0.0;
 
+    bool IsValid() const
+    {
+        return !Event.IsEmpty();
+    }
 };
