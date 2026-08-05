f841135a7826ba67dc663274dc12fc65a59c162f
jetsontech <56722744+jetsontech@users.noreply.github.com>
2026-08-05 17:07:45 -0400

    Connect bridge module to runtime manager singleton

--- Plugins/SarembokBridge/Source/SarembokBridge/Private/SarembokBridgeModule.cpp
+++ Plugins/SarembokBridge/Source/SarembokBridge/Private/SarembokBridgeModule.cpp
@@ -1,39 +1,29 @@
 #include "SarembokBridgeModule.h"
 #include "SarembokRuntimeManager.h"
 
-
 void FSarembokBridgeModule::StartupModule()
 {
+    UE_LOG(
+        LogTemp,
+        Display,
+        TEXT("Sarembok Bridge Initialized")
+    );
 
-UE_LOG(
-LogTemp,
-Display,
-TEXT("Sarembok Bridge Initialized")
-);
-
-
-USarembokRuntimeManager* Runtime =
-NewObject<USarembokRuntimeManager>();
-
-Runtime->InitializeRuntime();
-
+    FSarembokRuntimeManager::Get().Initialize();
 }
-
-
 
 void FSarembokBridgeModule::ShutdownModule()
 {
+    FSarembokRuntimeManager::Get().Shutdown();
 
-UE_LOG(
-LogTemp,
-Display,
-TEXT("Sarembok Bridge Shutdown")
-);
-
+    UE_LOG(
+        LogTemp,
+        Display,
+        TEXT("Sarembok Bridge Shutdown")
+    );
 }
 
-
 IMPLEMENT_MODULE(
-FSarembokBridgeModule,
-SarembokBridge
+    FSarembokBridgeModule,
+    SarembokBridge
 )
