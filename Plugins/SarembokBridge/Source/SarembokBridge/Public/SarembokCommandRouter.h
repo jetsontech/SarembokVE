5227152982a4b6a511d3c853850eea0cd2c64114
jetsontech <56722744+jetsontech@users.noreply.github.com>
2026-08-07 10:10:37 -0400

    Add Sarembok command routing layer

---
+++ Plugins/SarembokBridge/Source/SarembokBridge/Public/SarembokCommandRouter.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SarembokCommandRouter.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSarembokCommand
+{
+    GENERATED_BODY()
+
+    UPROPERTY(BlueprintReadWrite)
+    FString Command;
+
+    UPROPERTY(BlueprintReadWrite)
+    FString Target;
+
+    UPROPERTY(BlueprintReadWrite)
+    FString Payload;
+};
+
+UCLASS(Blueprintable)
+class SAREMBOKBRIDGE_API USarembokCommandRouter : public UObject
+{
+    GENERATED_BODY()
+
+public:
+
+    UFUNCTION(BlueprintCallable, Category="Sarembok Bridge")
+    bool RouteCommand(const FSarembokCommand& Command);
+
+    UFUNCTION(BlueprintCallable, Category="Sarembok Bridge")
+    FString GetLastCommand() const;
+
+private:
+
+    FString LastCommand;
+};
