ee5dc8d511683ce6d02d3393e7b3bb26564f704d
jetsontech <56722744+jetsontech@users.noreply.github.com>
2026-08-07 10:21:14 -0400

    Add Sarembok autonomous agent loop interface

---
+++ Plugins/SarembokAgent/Source/SarembokAgent/Public/SarembokAutonomousLoop.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SarembokAutonomousLoop.generated.h"
+
+UENUM(BlueprintType)
+enum class ESarembokLoopState : uint8
+{
+    Idle,
+    Perceiving,
+    Planning,
+    Acting,
+    Responding
+};
+
+UCLASS(Blueprintable)
+class SAREMBOKAGENT_API USarembokAutonomousLoop : public UObject
+{
+    GENERATED_BODY()
+
+public:
+
+    UFUNCTION(BlueprintCallable, Category="Sarembok AI")
+    void StartCycle(const FString& InputContext);
+
+    UFUNCTION(BlueprintCallable, Category="Sarembok AI")
+    ESarembokLoopState GetLoopState() const;
+
+private:
+
+    ESarembokLoopState CurrentState = ESareMBOKLoopState::Idle;
+    FString CurrentContext;
+};
