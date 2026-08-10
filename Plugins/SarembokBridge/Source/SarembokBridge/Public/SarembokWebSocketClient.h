940554c3baf5f5f81025786a9a733f399d909134
jetsontech <56722744+jetsontech@users.noreply.github.com>
2026-08-09 20:05:06 -0400

    Expose WebSocket receive delegate

--- Plugins/SarembokBridge/Source/SarembokBridge/Public/SarembokWebSocketClient.h
+++ Plugins/SarembokBridge/Source/SarembokBridge/Public/SarembokWebSocketClient.h
@@ -3,37 +3,30 @@
 #include "CoreMinimal.h"
 #include "IWebSocket.h"
 
+DECLARE_MULTICAST_DELEGATE_OneParam(FSarembokWebSocketMessageDelegate, const FString&);
+
 class FSarembokWebSocketClient
 {
 public:
+    FSarembokWebSocketClient();
+    ~FSarembokWebSocketClient();
 
-	FSarembokWebSocketClient();
+    void Connect();
+    void Disconnect();
+    void SendMessage(const FString& Message);
 
-	~FSarembokWebSocketClient();
-
-	void Connect();
-
-	void Disconnect();
-
-	void SendMessage(const FString& Message);
-
+    FSarembokWebSocketMessageDelegate& OnMessageReceived()
+    {
+        return MessageReceived;
+    }
 
 private:
+    void OnConnected();
+    void OnMessage(const FString& Message);
+    void OnConnectionError(const FString& Error);
+    void OnClosed(int32 StatusCode, const FString& Reason, bool bWasClean);
 
-	void OnConnected();
-
-	void OnMessage(const FString& Message);
-
-	void OnConnectionError(const FString& Error);
-
-	void OnClosed(
-		int32 StatusCode,
-		const FString& Reason,
-		bool bWasClean
-	);
-
-
-	TSharedPtr<IWebSocket> Socket;
-
-	FString ServerURL;
+    TSharedPtr<IWebSocket> Socket;
+    FString ServerURL;
+    FSarembokWebSocketMessageDelegate MessageReceived;
 };
